7e0034284b62473f08293ff57d56fa5cd1270264
Girish Sancheti <girish400@gmail.com>
2021-02-09 20:54:17 -0800

    ok

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+     print(x, y) 
+     for x in adj:
+        for y in fruits:
+            print(x, y)
